479e5966338f47d579cba1e78b141e7c925ed913
Logan Watkins <lw2014wa@gmail.com>
2023-08-25 20:44:48 -0500

    Removed extraneous value

--- binarySearch.js
+++ binarySearch.js
@@ -1,16 +1,15 @@
 function binarySearch(array, target) {
     let leftPointer = 0;
     let rightPointer = array.length - 1;
-    while(leftPointer <= rightPointer){
-      const midPointer = Math.floor((leftPointer + rightPointer) / 2);
-      const midValue = array[midPointer];
-      if(midValue === target){
-        return midPointer;
-      } else if(target > midValue){
-        leftPointer = midPointer + 1;
-      } else {
-        rightPointer = midPointer - 1;
-      }
+    while (leftPointer <= rightPointer) {
+        const midPointer = Math.floor((leftPointer + rightPointer) / 2);
+        if (array[midPointer] === target) {
+            return midPointer;
+        } else if (target > array[midPointer]) {
+            leftPointer = midPointer + 1;
+        } else {
+            rightPointer = midPointer - 1;
+        }
     }
     return -1;
-  }
+}
